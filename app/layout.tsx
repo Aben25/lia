0dc9397e228a89e8bcfb4b8e0f3d7230c9260c7c
Aben25 <abenuro@gmail.com>
2024-09-03 13:58:33 -0400

    title changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Love in Action - Sponsorship Portal",
+  description: "Sponsor a child and change their life",
 };
 
 export default function RootLayout({
