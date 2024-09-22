c36adff35e3a4efbbdbcc53174e70552b6fc271c
Samuel Addis <samueladdis@Samuels-MacBook-Pro.local>
2024-09-21 20:00:06 -0400

    secondary auth in the sign in form

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,11 +1,27 @@
+'use client';
+
 import { signInAction } from '@/app/actions';
+import EmailLogin from '@/components/email-login';
 import { FormMessage, Message } from '@/components/form-message';
+import PhoneLogin from '@/components/phone-login';
 import { SubmitButton } from '@/components/submit-button';
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import Link from 'next/link';
+import { useState } from 'react';
+
+type LoginType = 'email' | 'phone';
 
 export default function Login({ searchParams }: { searchParams: Message }) {
+  const [loginType, setLoginType] = useState<LoginType>('email');
+
+  const secondaryBtnLabel =
+    loginType === 'email' ? 'Sign in with phone' : 'Sign in with email';
+
+  const toggleLoginType = (): LoginType =>
+    loginType === 'email' ? 'phone' : 'email';
+
   return (
     <form className="flex-1 flex flex-col w-full sm:max-w-xs mx-0">
       <h1 className="text-2xl font-medium text-center">Sign in</h1>
@@ -15,33 +31,15 @@ export default function Login({ searchParams }: { searchParams: Message }) {
           Sign up
         </Link>
       </p>
-      <div className="flex flex-col gap-2 [&>input]:mb-3 [&>input]:text-base [&>input]:p-4 [&>input]:h-10  mt-8">
-        <Label htmlFor="email">Email</Label>
-        <Input name="email" placeholder="you@example.com" required />
-        <div className="flex justify-between items-center">
-          <Label htmlFor="password">Password</Label>
-          <Link
-            className="text-xs text-foreground underline"
-            href="/forgot-password"
-          >
-            Forgot Password?
-          </Link>
-        </div>
-        <Input
-          type="password"
-          name="password"
-          placeholder="Your password"
-          required
-        />
-        <SubmitButton
-          className="text-base h-14 sm:h-11 mt-2"
-          pendingText="Signing In..."
-          formAction={signInAction}
-        >
-          Sign in
-        </SubmitButton>
-        <FormMessage message={searchParams} />
-      </div>
+      {loginType === 'email' ? <EmailLogin /> : <PhoneLogin />}
+      <Button
+        variant="outline"
+        type="button"
+        onClick={() => setLoginType(toggleLoginType())}
+      >
+        {secondaryBtnLabel}
+      </Button>
+      <FormMessage message={searchParams} />
     </form>
   );
 }
