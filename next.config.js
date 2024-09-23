88a9659a1b26723c1ecaf05f89783b1af7e5ab16
Aben25 <51212800+Aben25@users.noreply.github.com>
2024-09-22 21:17:57 -0400

    format fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['qkehakucnmertrgjgqrk.supabase.co'],
-    },
-  };
+  images: {
+    domains: ['qkehakucnmertrgjgqrk.supabase.co'],
+  },
+};
 module.exports = nextConfig;
